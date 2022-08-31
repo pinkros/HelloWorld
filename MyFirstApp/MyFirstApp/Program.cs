cb6c376bc96496872fbd246cdbf3292219a6451e
pinkros <arvid.leimar@iths.se>
2022-08-31 08:48:16 +0200

    uppdatering, deklarerat variabler.

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello! My name is Arvid Leimar.");
+﻿//skriv in variabler för: namn, ålder, huruvida du ätit frukost idag
+
+var fullName = "Arvid Leimar";
+var age = 31;
+var breakfast = true;
+
+Console.WriteLine($"Hello! My name is {fullName}.");
+
